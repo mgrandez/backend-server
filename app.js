5ba08ec5cf83f0d1421e3d26cef17890adb98a43
Miguel <mgrandez@hotmail.com>
2018-06-02 18:43:42 -0500

    Usuario y Token listo

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
-
-
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -20,15 +29,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(403).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
-
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchard peticiones
